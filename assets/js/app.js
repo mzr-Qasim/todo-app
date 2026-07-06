915a46a75c15583a7259d6ae685db7644b90ec83
Qasim <malikqasim20051@gmail.com>
2026-07-07 03:17:35 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,70 @@
 const checkbox = document.getElementById("checkbox");
-const checkboxOffcanvas = document.getElementById("checkbox_2");
-const navBar = document.querySelector(".main-navbar");
+const navBar = document.querySelector(".navbar-inner");
 const mainHero = document.querySelector(".main-hero-section");
 const mainHeroHeading = document.querySelector(".hero-heading");
 const themeButton = document.querySelector(".button");
 
 checkbox.addEventListener("change", () => {
-    document.body.classList.toggle("dark-mode");
-    navBar.classList.toggle("dark-mode-navbar");
-    mainHero.classList.toggle("dark-mode-hero");
-    mainHeroHeading.classList.toggle("hero-heading-dark");
-    themeButton.classList.toggle("dark-mode-theme-button");
+  document.body.classList.toggle("dark-mode");
+  navBar.classList.toggle("dark-mode-navbar");
+  mainHero.classList.toggle("dark-mode-hero");
+  mainHeroHeading.classList.toggle("hero-heading-dark");
+  themeButton.classList.toggle("dark-mode-theme-button");
 });
 
-checkboxOffcanvas.addEventListener("change", () => {
-    document.body.classList.toggle("dark-mode");
-    navBar.classList.toggle("dark-mode-navbar");
-    mainHero.classList.toggle("dark-mode-hero");
-    mainHeroHeading.classList.toggle("hero-heading-dark");
-    themeButton.classList.toggle("dark-mode-theme-button");
+
+
+// Todo App
+
+
+const taskInput = document.getElementById("taskInput");
+const createButton = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+createButton.addEventListener("click", function () {
+
+
+    if (taskInput.value.trim() === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+
+    const li = document.createElement("li");
+
+
+    li.innerHTML = `
+        <p class="task-text">${taskInput.value}</p>
+
+        <div class="task-actions">
+            <button class="complete-btn">✓</button>
+            <button class="delete-btn">🗑</button>
+        </div>
+    `;
+
+
+    const completeBtn = li.querySelector(".complete-btn");
+    const deleteBtn = li.querySelector(".delete-btn");
+    const taskText = li.querySelector(".task-text");
+
+
+    completeBtn.addEventListener("click", function () {
+        li.classList.toggle("completed");
+        
+    });
+
+
+    deleteBtn.addEventListener("click", function () {
+        li.remove();
+    });
+
+
+    taskList.appendChild(li);
+
+
+    taskInput.value = "";
+
+
+    taskInput.focus();
+
 });
